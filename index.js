39a0001df34e1e6cb36fd62afb6c423d6aa7065e
GCdM <59828466+GCdM@users.noreply.github.com>
2020-09-26 09:47:05 +0100

    Basic structure for Express app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Testing");
+});
+
+app.listen(PORT, () => {
+  console.log(` --- Listening on port ${PORT} --- `);
+});
